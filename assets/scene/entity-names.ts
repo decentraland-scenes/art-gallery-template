445c265201f2834662ecfe8780e615b305dc99a7
agustincarriso <86750198+agustincarriso@users.noreply.github.com>
2025-08-08 00:29:11 -0300

    ft: add new models and images

--- assets/scene/entity-names.ts
+++ assets/scene/entity-names.ts
@@ -5,5 +5,14 @@
  * Object containing all entity names in the scene for autocomplete support.
  */
 export enum EntityNames {
+  _1st_floor_image_1 = "1st_floor_image_1",
+  _1st_floor_image_2 = "1st_floor_image_2",
+  _1st_floor_image_3 = "1st_floor_image_3",
+  _1st_floor_image_4 = "1st_floor_image_4",
+  _1st_floor_image_5 = "1st_floor_image_5",
+  _2nd_floor_image_1 = "2nd_floor_image_1",
+  _2nd_floor_image_1_2 = "2nd_floor_image_1_2",
+  Custom_Text = "Custom_Text",
+  Vertical_White_Pad = "Vertical White Pad",
   artgallerymats_e3_glb = "artgallerymats_e3.glb",
 } 
